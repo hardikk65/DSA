b589d240d497ecf78d4efa8dd4ce11b29cf71b27
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-01-23 16:20:21 +0530

    Time: 15 ms (35.76%) | Memory: 12.8 MB (5.03%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        map<char,int> roman{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+
+        int answer = 0;
+
+        for(int i = 0;i < s.size() - 1;i++){
+            if(roman[s[i + 1]] > roman[s[i]]){
+                answer = answer - roman[s[i]];
+            }
+            else{
+                answer = answer + roman[s[i]];
+            }
+
+        }
+        answer = answer + roman[s[s.size() - 1]];
+        return answer;
+
+        
+    }
+};

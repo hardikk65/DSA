5836920f1743f0d6ffad8a00652a11d74c9b2b27
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-01-23 16:29:35 +0530

    Time: 4 ms (87.19%) | Memory: 12.5 MB (5.03%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
 
-        map<char,int> roman{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        unordered_map<char,int> roman{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
 
         int answer = 0;
 

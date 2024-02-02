70d6e49b68a99659f2af61b01102e5cd216ced23
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-02 22:32:00 +0530

    Time: 4 ms (11.85%) | Memory: 8.2 MB (6.44%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,54 @@
+// class Solution {
+// public:
+//     double f1(double x, int n, double& o) {
+//         if (n > 0) {
+//             if (x >= 1 || x <= -1)
+//                 o = x * o;
+//             else {
+//                 if (o > pow(10, -15)) {
+//                     o = x * o;
+
+//                 } else {
+//                     return 0;
+//                 }
+//             }
+
+//         } else {
+//             return o;
+//         }
+//         f1(x, n - 1, o);
+//         return o;
+//     }
+//     double f2(double x, int n, double& o) {
+//         if (n < 0) {
+//             if (o > pow(10, -15)) {
+//                 o = (1 / x) * o;
+
+//             } else {
+//                 return 0;
+//             }
+//         } else {
+//             return o;
+//         }
+//         f2(x, n + 1, o);
+//         return o;
+//     }
+//     double myPow(double x, int n) {
+//         double o = 1;
+//         if (n > 0) {
+//             o = f1(x, n, o);
+//         } else {
+//             o = f2(x, n, o);
+//         }
+
+//         return o;
+//     }
+// };
+class Solution {
+public:
+    double myPow(double x, int n) {
+		double s = 1;
+		s = (double)pow(x, n);
+        return s ;
+    }
+};

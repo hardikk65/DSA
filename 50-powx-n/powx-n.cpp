7992c45c15fc379de4ce6415fe8c214c1a3aeab9
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-04 18:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.72%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,54 +1,13 @@
-// class Solution {
-// public:
-//     double f1(double x, int n, double& o) {
-//         if (n > 0) {
-//             if (x >= 1 || x <= -1)
-//                 o = x * o;
-//             else {
-//                 if (o > pow(10, -15)) {
-//                     o = x * o;
-
-//                 } else {
-//                     return 0;
-//                 }
-//             }
-
-//         } else {
-//             return o;
-//         }
-//         f1(x, n - 1, o);
-//         return o;
-//     }
-//     double f2(double x, int n, double& o) {
-//         if (n < 0) {
-//             if (o > pow(10, -15)) {
-//                 o = (1 / x) * o;
-
-//             } else {
-//                 return 0;
-//             }
-//         } else {
-//             return o;
-//         }
-//         f2(x, n + 1, o);
-//         return o;
-//     }
-//     double myPow(double x, int n) {
-//         double o = 1;
-//         if (n > 0) {
-//             o = f1(x, n, o);
-//         } else {
-//             o = f2(x, n, o);
-//         }
-
-//         return o;
-//     }
-// };
 class Solution {
 public:
-    double myPow(double x, int n) {
-		double s = 1;
-		s = pow(x, n);
-        return s ;
+    double myPow(double x, long long n) {
+        n = static_cast<long long> (n);
+        if(n==0) return 1;
+        if(n<0) return 1/myPow(x, -n);
+        if(n%2){
+            return x*myPow(x*x, n/2);
+        }
+
+        return myPow(x*x, n/2);
     }
 };

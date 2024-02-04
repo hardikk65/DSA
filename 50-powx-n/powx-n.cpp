5b033d5058df1371c9436673694977f7d5a33ba7
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-04 18:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (21.51%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     double myPow(double x, long long n) {
-        n = static_cast<long long>(n);
+        // n = static_cast<long long>(n);
         if (n == 0) return 1;
         if (n < 0) return myPow(1 / x, abs(n));
         
         if (n % 2 == 0) {
-                        double temp = myPow(x, n / 2);
+            double temp = myPow(x, n / 2);
             return temp * temp;
 
-        } else {
+        }
+        else{
             return x * myPow(x, n - 1);
         }
     }

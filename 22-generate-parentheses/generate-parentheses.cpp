a2fa955edce1904a4b4e89411ebb238809d48a25
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-09 19:13:12 +0530

    Time: 3 ms (65.33%) | Memory: 20.1 MB (7.08%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     void parent(vector<string>& answer,string s,int open,int close,int digi){
-    if((s.size() == digi) && open-close != 0){
+    if((s.size() == digi) && open-close != 0 || (open - close < 0)){
         return;
     }
     if((s.size() == digi) && open - close == 0){
         answer.push_back(s);
         return;
     }
-    if(open - close < 0){
-        return;
-    }
     parent(answer,s + '(',open + 1,close,digi);
     parent(answer,s + ')',open,close + 1,digi);
-    return;
 }
     vector<string> generateParenthesis(int n) {
         vector<string> answer;

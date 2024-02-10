a9313ce2f1bae5a5a5ceb7ed4e3f56e1a01ad62c
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-10 19:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (48.75%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,42 +1,25 @@
 class Solution {
+
 public:
-    void calcsum(vector<int>& candidates,vector<vector<int>>& answer,vector<int> ans,int number, int target,int n, int curr){
-        
-        ans.push_back(number); 
-        curr = curr + number;       
-        if(curr==target)
-        {
-            answer.push_back(ans);
+    void calcsum(vector<vector<int>>& answer, vector<int>& nums, int index, int target, int sum, vector<int>& candidates) {
+        if (sum == target) {
+            answer.push_back(nums);
+            return;
+        }
+        if (sum > target || index == candidates.size()) {
             return;
         }
 
-        if(curr>target)
-        {
-            return;
-        }
+        nums.push_back(candidates[index]);
+        calcsum(answer, nums, index, target, sum + candidates[index], candidates);
+        nums.pop_back();
 
-        for(int i=n; i<candidates.size();i++)
-        {
-            calcsum(candidates,answer,ans,candidates[i],target,i,curr);
-        }
-
-
-
+        calcsum(answer, nums, index + 1, target, sum, candidates);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-
         vector<vector<int>> answer;
-        vector<int> ans;
-
-
-        for(int i =0 ; i < candidates.size();i++){
-            calcsum(candidates,answer,ans,candidates[i],target,i,0);
-        }
-
+        vector<int> nums;
+        calcsum(answer, nums, 0, target, 0, candidates);
         return answer;
-
-
-        
     }
 };

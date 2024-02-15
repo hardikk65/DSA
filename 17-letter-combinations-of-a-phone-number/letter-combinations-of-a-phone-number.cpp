8d9ee65a3e657c056dd76a4b0b565fd8cb3e1211
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-15 16:27:52 +0530

    Time: 4 ms (11.21%) | Memory: 11.1 MB (5.05%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void combinations(vector<string>& answer,string s, int i,char p,int size,map<char,string> alphabets,string digits){
+    void combinations(vector<string>& answer,string s, int i,char p,map<char,string> alphabets,string digits){
         if(p != ' ')s+=p;
-        if(s.size() == size){
+        if(s.size() == digits.size()){
             answer.push_back(s);
             return;
         }
         for(int k = 0;k < alphabets[digits[i]].size();k++){
             cout<<digits[i]<<endl;
-            combinations(answer,s,i + 1,alphabets[digits[i]][k],size,alphabets,digits);
+            combinations(answer,s,i + 1,alphabets[digits[i]][k],alphabets,digits);
         }
         return;
     }
@@ -27,7 +27,7 @@ public:
         alphabets['8'] = "tuv";
         alphabets['9'] = "wxyz";
 
-        combinations(answer,"",0,' ',digits.size(),alphabets,digits);
+        combinations(answer,"",0,' ',alphabets,digits);
 
         return answer;
         

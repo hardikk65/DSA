a619df0e3631e93d9e90de41f8efc168b1165272
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-02-18 01:45:39 +0530

    Time: 134 ms (32.54%) | Memory: 115.4 MB (24.41%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void combination(vector<vector<string>>& answer,vector<string> temp,string s){
+        if(s.size() == 0){
+            return;
+        }
+        if(s.size()==1){
+            temp.push_back(s);
+            answer.push_back(temp);
+            return ;
+        }
+      
+        for(int i= 1 ;i<=s.size();i++){
+            string og = s.substr(0,i);
+            reverse(og.begin(),og.end());
+            if(i == s.size() && og == s.substr(0,i)){
+                temp.push_back(og);
+                answer.push_back(temp);
+                break;
+            }
+            if(og == s.substr(0,i)){
+                temp.push_back(og);
+                combination(answer,temp,s.substr(i,s.size() - i));
+                temp.pop_back();
+                
+            }
+        }
+        return;
+
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> answer;
+        vector<string> temp;
+        combination(answer,temp,s);
+
+        return answer;
+        
+    }
+};

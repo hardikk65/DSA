be87b7a619d9bc28ef027babe3e8a64d0f7a2318
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-05-30 14:10:28 +0530

    Time: 126 ms (69.14%) | Memory: 98.7 MB (36.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    bool f(int index, int sa, int sb, vector<int>& nums,vector<vector<int>>& dp){
-        if(index == nums.size())return (sa == sb);
+    bool f(int index, int sa,vector<int>& nums,vector<vector<int>>& dp){
+        if(sa == 0)return true;
+        if(index == 0)return (nums[0] == sa);
         if(dp[index][sa] != -1)return dp[index][sa];
-        return dp[index][sa] = f(index + 1,sa + nums[index],sb,nums,dp) | f(index + 1,sa,sb + nums[index],nums,dp);
+
+        bool nt = f(index - 1, sa,nums,dp);
+        bool t = false;
+
+        if(sa >= nums[index]){
+            t = f(index - 1,sa - nums[index],nums,dp);
+        }
+        return dp[index][sa] = t | nt;
     }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i = 0;i < nums.size();i++)sum += nums[i];
-        vector<vector<int>> dp(nums.size(),vector<int> (sum + 1,-1));
-        return f(0,0,0,nums,dp);
+        if(sum % 2 != 0)return false;
+        vector<vector<int>> dp(nums.size(),vector<int> ((sum/2) + 1,-1));
+        return f(nums.size() - 1,sum/2,nums,dp);
         
     }
 };

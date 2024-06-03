52683d777b50b7b7a2821593a5ceb6038da81eb9
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-06-03 13:43:57 +0530

    Time: 4 ms (37.99%) | Memory: 8.2 MB (78.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    void powerset(vector<vector<int>>& answer,vector<int> nums){
-        int number = nums[nums.size() - 1];
-        if(nums.size() == 1){
-            answer.push_back(nums);
-            return;
-        }
-        nums.pop_back();
-        powerset(answer,nums);
-        int size = answer.size();
-        vector<int> temp;
-        for(int i = 0;i < size;i++){
-            temp = answer[i];
-            temp.push_back(number);
-            answer.push_back(temp);
-        }
-        answer.push_back({number});
-        return;
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> answer;
-        powerset(answer,nums);
-        answer.push_back({});
-        return answer;
+        vector<vector<int>> powerset;
+        for(int i = 0;i < 1<<nums.size();i++){
+            int a = i;
+            int count = 0;
+            vector<int> ans;
+            while(a != 0){
+                if(a&1)ans.push_back(nums[count]);
+                a = a>>1;
+                count++;
+            }
+            powerset.push_back(ans);
+        }
+        return powerset;
 
-
+        
     }
 };

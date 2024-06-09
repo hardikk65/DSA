062bedfe9793602b445a0077243b9c748c88f90e
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-06-10 02:08:35 +0530

    Time: 51 ms (31.16%) | Memory: 50.9 MB (22.98%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> dp(nums.size(),0);
+        // vector<bool> dp(nums.size(),0);
     
         if(nums.size() == 1)return true;
         int mov = nums.size() -2;
         int nearest_ptr = nums.size() - 1;
         while(mov >= 0){
             if(nums[mov] >= nearest_ptr - mov){
-                dp[mov] = 1;
+                // dp[mov] = 1;
                 nearest_ptr = mov;
             }
             mov--;
         }
-        return dp[0];
+        return (mov + 1 == nearest_ptr);
         
     }
 };

2bc5709fc990928e6b010860090e79deec90e050
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-07-03 19:01:42 +0530

    Time: 851 ms (11.45%) | Memory: 10.7 MB (83.88%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -5,14 +5,11 @@ public:
             if(2*currsum - totalsum == target)return 1;
             return 0;
         }
-        // if(dp[index][currsum] != -1)return dp[index][currsum];
-
         return targetsum(index + 1,currsum + nums[index],nums,target,totalsum) + targetsum(index + 1,currsum,nums,target,totalsum);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int totalsum = 0;
         for(auto c : nums)totalsum += c;
-        // vector<vector<int>> dp(nums.size(),vector<int>(totalsum,-1));
         return targetsum(0,0,nums,target,totalsum);
         
     }

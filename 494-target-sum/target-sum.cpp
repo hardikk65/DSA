6e459be918aab973c920f6ca7938881328aff5ab
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-07-03 19:09:14 +0530

    Time: 1236 ms (5.01%) | Memory: 17.8 MB (31.84%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int targetsum(int index,int currsum,vector<int>& nums,int target,int totalsum){
+    int targetsum(int index,int currsum,vector<int>& nums,int target,int totalsum,vector<vector<int>>& dp){
         if(index == nums.size()){
             if(2*currsum - totalsum == target)return 1;
             return 0;
         }
-        return targetsum(index + 1,currsum + nums[index],nums,target,totalsum) + targetsum(index + 1,currsum,nums,target,totalsum);
+        if(dp[index][currsum] != -1)return dp[index][currsum];
+
+        return targetsum(index + 1,currsum + nums[index],nums,target,totalsum,dp) + targetsum(index + 1,currsum,nums,target,totalsum,dp);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int totalsum = 0;
         for(auto c : nums)totalsum += c;
-        return targetsum(0,0,nums,target,totalsum);
+        vector<vector<int>> dp(nums.size(),vector<int>(totalsum + 1,-1));
+        return targetsum(0,0,nums,target,totalsum,dp);
         
     }
 };

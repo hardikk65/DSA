7a2bd4bd2a58978d738eeb6aae20d22d048335e8
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-07-03 20:44:53 +0530

    Time: 13 ms (42.83%) | Memory: 20.1 MB (22.36%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int leftamount,int index,vector<int>& coins,vector<vector<int>>& dp){
+        if(leftamount == 0)return 1;
+        if(dp[index][leftamount] != -1)return dp[index][leftamount];
+
+        int answer = 0;
+        for(int i = index;i >= 0;i--){
+            if(coins[i] <= leftamount){
+                answer += f(leftamount - coins[i],i,coins,dp);
+            }
+        }
+        // if(answer == INT_MAX)return dp[index][leftamount] = INT_MAX;
+
+        return dp[index][leftamount] = answer;
+    }
+    int change(int amount, vector<int>& coins) {
+        
+        vector<vector<int>> dp(coins.size(),vector<int>(amount + 1,-1));
+        return f(amount,coins.size() - 1,coins,dp);
+        
+    }
+};

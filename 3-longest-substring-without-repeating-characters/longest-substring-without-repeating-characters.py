a1feacb1af898c277c826d2afe0d663d1e49bb0d
Hardik <119474920+hardikk65@users.noreply.github.com>
2024-07-13 13:58:15 +0530

    Time: 50 ms (80.70%) | Memory: 16.6 MB (76.97%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,17 +3,23 @@ class Solution:
         if len(s) == 0: # If input string is empty return 0
             return 0
 
-        maxlen = 0  # Stores the maximum length of substring. Initialised with -1
-        set_a = set()
-        l = 0
+        maxlen = 0  # Stores the maximum length of substring
+        set_a = set()  # Stores the characters of substring
 
-        for r in range(len(s)):
-            if s[r] in set_a:
-                while l < r and s[r] in set_a:
-                    set_a.remove(s[l])
-                    l += 1
-            set_a.add(s[r])
-            maxlen = max(maxlen,r-l + 1)
+        # left stores starting index, right stores end index of substring
+        left = 0  
+
+        for right in range(len(s)):  # Traversing the input string
+            
+            if s[right] in set_a:   # If current character present in set
+                
+                while left < right and s[right] in set_a:  # Remove until duplicate
+                    
+                    set_a.remove(s[left])
+                    left += 1
+                    
+            set_a.add(s[right])  # Add current character to set
+            maxlen = max(maxlen,right-left + 1) # Store max lenght in maxlen
             
         return maxlen
         
